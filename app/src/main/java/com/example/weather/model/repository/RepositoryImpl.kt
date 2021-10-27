0b1099e20d0fd01a03ea1aac914a1c6877f3f2ef
Peach <ipeachyou@yandex.ru>
2021-10-27 16:16:19 +0500

    картинка

--- app/src/main/java/com/example/weather/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/weather/model/repository/RepositoryImpl.kt
@@ -3,13 +3,14 @@ package com.example.weather.model.repository
 import com.example.weather.model.WeatherLoader
 import com.example.weather.model.enitities.City
 import com.example.weather.model.enitities.Weather
+import com.example.weather.model.rest.WeatherRepo
 
 class RepositoryImpl : Repository {
     override fun getWeatherFromServer(lat: Double, lng: Double): Weather{
-        val dto = WeatherLoader.loadWeather(lat, lng)
+        val dto = WeatherRepo.api.getWeather(lat, lng).execute().body()
         return Weather(
             temperature = dto?.fact?.temp ?: 0,
-            feelsLike = dto?.fact?.feels_like ?: 0,
+            feelsLike = dto?.fact?.feelsLike ?: 0,
             condition = dto?.fact?.condition,
         )
     }
